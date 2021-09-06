29533e4195a0b6dbb3d6f68b66e33ecb71c48dbe
Matheus Garcia <matheusgarcia.developer@gmail.com>
2021-09-06 19:11:35 -0300

    final project

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+
+
+/**
+ * Database setup
+ */
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(cors()); //permite o trabalho com portas , uma vez que trabalho com back e front
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+); //me dá acesso no localhost, pela route /files
+//http://localhost:3000/files/92ae39cfcb0c0024aa912fe607c287bf-WhatsApp%20Image%202019-05-19%20at%2018.54.21%20(1).jpeg
+// o caminho acimame dá uma imagem!
+
+app.use(require("./routes"));
+
+app.listen(3000);
---
+++ src/models/Post.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const aws = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+
+const s3 = new aws.S3();
+
+const PostSchema = new mongoose.Schema({
+  name: String,
+  size: Number,
+  key: String,
+  url: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+PostSchema.pre("save", function() {
+  if (!this.url) {
+    this.url = `${process.env.APP_URL}/files/${this.key}`;
+  } //se não tem URL, ou seja, se eu salvo no storage, eu darei o caminho localhost (ou a URL definida em .env) e crio uma route
+});
+
+PostSchema.pre("remove", function() {
+  if (process.env.STORAGE_TYPE === "s3") { //faço essa função para o caso de storage s3
+    return s3
+      .deleteObject({
+        Bucket: process.env.BUCKET_NAME,
+        Key: this.key
+      })
+      .promise()
+      .then(response => {
+        console.log(response.status);
+      })
+      .catch(response => {
+        console.log(response.status);
+      });
+  } else {
+    return promisify(fs.unlink)( //deleta imagens locais! 
+      path.resolve(__dirname, "..", "..", "tmp", "uploads", this.key)
+    );
+  }
+});
+
+module.exports = mongoose.model("Post", PostSchema);
